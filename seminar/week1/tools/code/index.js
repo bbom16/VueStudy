1c89520ec94393225f95119dc472050c3fd0887e
GuBeomJeong <jgb625@naver.com>
2017-09-10 16:57:53 +0900

    Update week1

---
+++ seminar/week1/tools/code/index.js
@@ -0,0 +1,8 @@
+import Vue from "vue"
+import './style.css';
+import {app} from './vueTest'
+
+new Vue(app);
+
+
+
--- seminar/week1/tools/code/vueTest.js
+++ seminar/week1/tools/code/vueTest.js
@@ -1,4 +1,4 @@
-export let app = {
+export const app = {
     el: '#app',
     data: {
         message: '안녕하세요 Vue!'
