b525254cad6322208b81b97fb4f622705a9cf902
yk1028 <wongeunsong94@gmail.com>
2017-09-17 19:01:29 +0900

    vue-template

---
+++ seminar/week2/vue-template/webpack.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    entry: {
+        "index" : './index.js'
+    },
+
+    output: {
+        path: __dirname + '/bundle/',
+        filename: '[name].bundle.js'
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        }
+    },
+    module: {
+        rules: [
+         {
+            test: /\.css$/,
+            use: [
+              'style-loader',
+              'css-loader'
+            ]
+         },
+         {
+             test: /\.js$/,
+             exclude: /node_modules/,
+             loader: 'babel-loader',
+             options: {
+               presets: ['env', "stage-2"]
+             }
+           }
+        ]
+    }
+}
